f3ac0528c734b0643d1724b2dd2502eda50772a4
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2024-12-01 20:24:45 +0000

    add winter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,25 @@ import GlobalStyles from '../globalStyles';
 import { ThemeProvider } from 'styled-components';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { Floating } from '@janmarkuslanger/floatingjs';
 
 function App({ Component, pageProps }: AppProps) {
     const router = useRouter();
 
     useEffect(() => {
+        const floating = new Floating({
+            elements: ['❄️'],
+            maxElements: 20,
+            interval: 1000,
+        });
+
+        floating.updateRoot(document.querySelector('body') || document.body);
+        floating.start();
+
         const handleRouteChange = (url: string) => {
+            floating.updateRoot(document.querySelector('body') || document.body);
+            floating.start();
+
             if (!('gtag' in window)) {
                 return;
             }
